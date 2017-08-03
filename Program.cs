8c2ecd21582cac06ff11ea9d326641153d52ec8c
Charles Pao <windperson@gmail.com>
2017-08-03 13:33:26 +0800

    add source code

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Novacode;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = @"test.docx";
+
+            var doc = DocX.Create(fileName);
+
+            Console.WriteLine($" docx file {fileName} created.");
+
+            var headLineFormat = new Formatting();
+            headLineFormat.FontFamily = new Font("Arial Black");
+            headLineFormat.Size = 18D;
+            headLineFormat.Position = 12;
+            var head = doc.InsertParagraph("This is title", false,headLineFormat);
+
+            var para = doc.InsertParagraph();
+            para.Append("Programmable insert text.");
+
+            Console.WriteLine("text inserted.");
+
+            doc.Save();
+
+            Console.WriteLine("document saved.");
+        }
+    }
+}
